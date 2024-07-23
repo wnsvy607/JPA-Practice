22668581b8daee88688862c88117d570a8f4051a
Lee Junpyo <wnsvy607@naver.com>
2024-07-23 11:35:47 +0900

    캐싱 설정 변경

--- src/main/kotlin/com/practice/jpa/config/EhCacheConfig.kt
+++ src/main/kotlin/com/practice/jpa/config/EhCacheConfig.kt
@@ -1,52 +1,49 @@
-package com.practice.jpa.config;
+package com.practice.jpa.config
 
-import java.time.Duration;
-import java.util.List;
-
-import javax.cache.Caching;
-import javax.cache.spi.CachingProvider;
-
-import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.ExpiryPolicyBuilder;
-import org.ehcache.config.builders.ResourcePoolsBuilder;
-import org.ehcache.config.units.MemoryUnit;
-import org.ehcache.jsr107.Eh107Configuration;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.SimpleKey;
-import org.springframework.cache.jcache.JCacheCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.ehcache.config.builders.CacheConfigurationBuilder
+import org.ehcache.config.builders.ExpiryPolicyBuilder
+import org.ehcache.config.builders.ResourcePoolsBuilder
+import org.ehcache.config.units.MemoryUnit
+import org.ehcache.jsr107.Eh107Configuration
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.interceptor.SimpleKey
+import org.springframework.cache.jcache.JCacheCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import java.time.Duration
+import javax.cache.Caching
 
 @Configuration
 @EnableCaching
-public class EhCacheConfig {
+class EhCacheConfig {
 
-	@Primary
-	@Bean
-	public CacheManager localCacheManager() {
-		return new JCacheCacheManager(ehCacheManager());
-	}
+    @Primary
+    @Bean
+    fun localCacheManager(): CacheManager {
+        return JCacheCacheManager(ehCacheManager())
+    }
 
-	private javax.cache.CacheManager ehCacheManager() {
-		CachingProvider provider = Caching.getCachingProvider();
-		javax.cache.CacheManager cacheManager = provider.getCacheManager();
+    private fun ehCacheManager(): javax.cache.CacheManager {
+        val provider = Caching.getCachingProvider()
+        val cacheManager = provider.cacheManager
 
-		javax.cache.configuration.Configuration<SimpleKey, List> tagTypeCacheConfiguration =
-			Eh107Configuration.fromEhcacheCacheConfiguration(config());
-		cacheManager.createCache("Tag", tagTypeCacheConfiguration);
+        val userCacheConfiguration =
+            Eh107Configuration.fromEhcacheCacheConfiguration(config())
+        cacheManager.createCache("User", userCacheConfiguration)
 
-		return cacheManager;
-	}
+        return cacheManager
+    }
 
-	private CacheConfigurationBuilder<SimpleKey, List> config() {
-		return CacheConfigurationBuilder.newCacheConfigurationBuilder(
-				SimpleKey.class,
-				List.class,
-				ResourcePoolsBuilder
-					.heap(100)
-					.offheap(10, MemoryUnit.MB))
-			.withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(1)));
-	}
+    private fun config(): CacheConfigurationBuilder<SimpleKey, MutableList<*>>? {
+        return CacheConfigurationBuilder.newCacheConfigurationBuilder(
+            SimpleKey::class.java,
+            MutableList::class.java,
+            ResourcePoolsBuilder
+                .heap(100)
+                .offheap(10, MemoryUnit.MB)
+        )
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(1)))
+    }
 }
